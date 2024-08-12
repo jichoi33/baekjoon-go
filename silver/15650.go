90ddb846fe23d4761124f62297525f3fb954e5b5
Jongin Choi <jonginchoi99@gmail.com>
2024-08-12 20:06:37 +0900

    08/12/2024

---
+++ silver/15650.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var (
+	scanner = bufio.NewScanner(os.Stdin)
+	writer  = bufio.NewWriter(os.Stdout)
+)
+
+var (
+	n, m   int
+	answer []int
+)
+
+func solve(at, depth int) {
+	if depth == m {
+		for i := 0; i < m; i++ {
+			writer.WriteString(strconv.Itoa(answer[i]))
+			writer.WriteByte(' ')
+		}
+		writer.WriteByte('\n')
+		return
+	}
+
+	for i := at; i <= n; i++ {
+		answer[depth] = i
+		solve(i+1, depth+1)
+	}
+}
+
+func scanInt() int {
+	scanner.Scan()
+	num, _ := strconv.Atoi(scanner.Text())
+	return num
+}
+
+func main() {
+	scanner.Split(bufio.ScanWords)
+	defer writer.Flush()
+
+	n, m = scanInt(), scanInt()
+
+	answer = make([]int, m)
+
+	solve(1, 0)
+}
