f746bda289e89f423b191cac95394a512a23beb1
Jongin Choi <jonginchoi99@gmail.com>
2024-08-18 22:15:45 +0900

    08/18/2024

---
+++ bronze/11654.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var (
+	scanner = bufio.NewScanner(os.Stdin)
+	writer  = bufio.NewWriter(os.Stdout)
+)
+
+func scanString() string {
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func main() {
+	scanner.Split(bufio.ScanWords)
+	defer writer.Flush()
+
+	r := scanString()[0]
+
+	writer.WriteString(strconv.Itoa(int(r)))
+	writer.WriteByte('\n')
+}
